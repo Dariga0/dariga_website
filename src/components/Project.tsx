35efa2a765e40392733e92f81722381dacbb333c
abylaydospayev <abylaidospayev@gmail.com>
2025-06-10 15:56:00 -0700

    added the button instead of href

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,25 +4,34 @@ import project_berlin from '../assets/images/Berling.png';
 import lspace from '../assets/images/lspace.png';
 
 function Project() {
-    return(
+  return (
     <div className="projects-container" id="projects">
-        <h1>Personal Projects</h1>
-        <div className="projects-grid">
-            <div className="project">
-                <a href="#" target="_blank" rel="noreferrer"><img src={project_berlin} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a  href="#" target="_blank" rel="noreferrer"><h2>Growing with AI</h2></a>
-                <p>Improved CRM and public relations with AI and Microsoft Co-Pilot. Optimized data management and explored
-opportunities for AI integration in service learning.</p>
-            </div>
-            <div className="project">
-                <a  href="#" target="_blank" rel="noreferrer"><img src={lspace} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a  href="#" target="_blank" rel="noreferrer"><h2>NASA MCA Academy</h2></a>
-                <p>Led a team of 12 in spacecraft systems research and mission design. Authored 80+ page proposal and delivered final
-presentation.</p>
-            </div>
+      <h1>Personal Projects</h1>
+      <div className="projects-grid">
+        <div className="project">
+          <button type="button" className="project-button">
+            <img src={project_berlin} className="zoom" alt="Growing with AI" width="100%" />
+            <h2>Growing with AI</h2>
+          </button>
+          <p>
+            Improved CRM and public relations with AI and Microsoft Co-Pilot. Optimized data management and explored
+            opportunities for AI integration in service learning.
+          </p>
         </div>
+
+        <div className="project">
+          <button type="button" className="project-button">
+            <img src={lspace} className="zoom" alt="NASA MCA Academy" width="100%" />
+            <h2>NASA MCA Academy</h2>
+          </button>
+          <p>
+            Led a team of 12 in spacecraft systems research and mission design. Authored 80+ page proposal and delivered final
+            presentation.
+          </p>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Project;
